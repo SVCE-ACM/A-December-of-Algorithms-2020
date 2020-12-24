075c427cda719d63a334f22abcaf6096e5f31f82
anusha-devulapally <64191013+anusha-devulapally@users.noreply.github.com>
2020-12-24 16:57:55 +0530

    Add files via upload

---
+++ December-13/python3_anusha_devulapally_Check_your_Spelling_Sara.py
@@ -0,0 +1,23 @@
+
+correct_word=list(map(str,input().split()))
+
+correct_word
+
+misspelt_words=input()
+
+def matchornot(correct,wrong):
+  m=len(correct)
+  n=len(wrong)
+  if(m!=n):
+    return 0
+  total=0
+  for i in range(m):
+    if(correct[i]!=wrong[i]):
+      total+=1
+  return total
+
+for i in correct_word:
+  val=matchornot(i,misspelt_words)
+  if(val==1):
+    print(i)
+
