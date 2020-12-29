b0e17f92e82acec417be7fefab43d84b7e05e2d3
anusha-devulapally <64191013+anusha-devulapally@users.noreply.github.com>
2020-12-29 12:21:14 +0530

    Add files via upload

---
+++ December-25/python3_anusha_devulapally_trapping_rain_water.py
@@ -0,0 +1,19 @@
+height=list(map(int,input().split()))
+
+height
+
+def trapped_water(h):
+  n=len(h)
+  total=0
+  for i in range(1,n-1):
+    left=h[i]
+    for j in range(i):
+      left=max(h[j],left)
+    right=h[i]
+    for j in range(i+1,n):
+      right=max(h[j],right)
+    total+=min(left,right)-h[i]
+  return total
+
+print(trapped_water(height))
+
