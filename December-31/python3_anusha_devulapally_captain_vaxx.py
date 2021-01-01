974c86bde735a704d78907cfba1a65f17f1dbe3b
anusha-devulapally <64191013+anusha-devulapally@users.noreply.github.com>
2021-01-02 00:47:29 +0530

    Add files via upload

---
+++ December-31/python3_anusha_devulapally_captain_vaxx.py
@@ -0,0 +1,40 @@
+x,y,len=map(int,input().split())
+
+coord=[]
+while(True):
+  k=list(map(int,input().split()))
+  if(k==[]):
+    break
+  coord.append(k)
+
+print(coord)
+
+import sys
+min=sys.maxsize
+li=list()
+for i in coord:
+  if(i[2]>len):
+    l=abs(x-i[0])+abs(y-i[1])
+    if(l<min):
+      min=l
+      li=i
+
+if(li[0]>0):
+  print('Right',end=' ')
+  print('Straight '*li[0],end='')
+  if(li[1]>0):
+    print('Left',end=' ')
+    print('Straight '*li[1],end='')
+  elif(li[1]<0):
+    print('Right',end=' ')
+    print('Straight'*li[1],end='')
+elif(li[0]<0):
+  print('Left',end=' ')
+  print('Straight'*li[0],end=' ')
+  if(li[1]>0):
+    print('Right',end=' ')
+    print('Straight '*li[1],end='')
+  elif(li[1]<0):
+    print('Left',end=' ')
+    print('Straight'*li[1],end='')
+
