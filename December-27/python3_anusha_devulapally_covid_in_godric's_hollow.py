813a4ff94892929647d04f2a7728f4aa4100a697
anusha-devulapally <64191013+anusha-devulapally@users.noreply.github.com>
2021-01-02 22:01:28 +0530

    Add files via upload

---
+++ December-27/python3_anusha_devulapally_covid_in_godric's_hollow.py
@@ -0,0 +1,31 @@
+n=int(input())
+
+import numpy as np
+def distance(x,y):
+  return round(np.sqrt(x**2+y**2))
+
+yes=[]
+no=[]
+for i in range(n):
+  x,y,re=map(str,input().split())
+  x=int(x)
+  y=int(y)
+  k=distance(x,y)
+  if (re=='Y'):
+    yes.append(k)
+  else:
+    no.append(k)
+
+print(yes)
+print(no)
+
+ye=max(yes)
+count=0
+for i in no:
+  if(i<=ye):
+    count+=1
+if (count==0):
+  print("No lockdown required")
+else:
+  print("Lockdown required")
+
