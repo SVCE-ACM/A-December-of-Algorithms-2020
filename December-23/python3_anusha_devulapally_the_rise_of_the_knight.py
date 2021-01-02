7872de72fffe5b6430da36cd91011c7bb80b695a
anusha-devulapally <64191013+anusha-devulapally@users.noreply.github.com>
2021-01-02 19:22:20 +0530

    Add files via upload

---
+++ December-23/python3_anusha_devulapally_the_rise_of_the_knight.py
@@ -0,0 +1,32 @@
+m,n=map(int,input().split())
+
+mat=[]
+for i in range(m):
+    k=list(map(int,input().split()))
+    mat.append(k)
+
+mat
+
+count=mat[0][0]
+i=0
+j=0
+
+while(True):
+  if (i==m-1 and j==n-1):
+    break
+  if (i<m-1 and j<n-1):
+    if (mat[i+1][j]>mat[i][j+1]):
+      count+=mat[i+1][j]
+      i+=1
+    else:
+      count+=mat[i][j+1]
+      j+=1
+  elif (i==m-1 and j<=n-1):
+    count+=mat[i][j+1]
+    j+=1
+  elif (j==n-1 and i<=m-1):
+    count+=mat[i+1][j]
+    i+=1
+
+print(abs(count)+1)
+
