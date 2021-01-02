861a8ea58735045b189e0df283d53b9798676ea0
anusha-devulapally <64191013+anusha-devulapally@users.noreply.github.com>
2021-01-02 18:42:52 +0530

    Add files via upload

---
+++ December-17/python3_anusha_devulapally_pokemon.py
@@ -0,0 +1,18 @@
+n=int(input())
+
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+
+a=sorted(a)
+b=sorted(b)
+
+def pokemon(a,b):
+  if(len(a)==0 or len(b)==0):
+    return 0
+  if (a[0]>b[0]):
+    return 1+pokemon(a[1:],b[1:])
+  else:
+    return pokemon(a[1:],b)
+
+print(pokemon(a,b))
+
