a56b02f282a27ee1f0b0db94558b0f039229fd5c
anusha-devulapally <64191013+anusha-devulapally@users.noreply.github.com>
2021-01-02 22:42:00 +0530

    Add files via upload

---
+++ December-19/python3_anusha_devulapally_war_on_wakanda.py
@@ -0,0 +1,21 @@
+inp=list(map(int,input().split()))
+
+maxi=int(input())
+
+c=1
+count=0
+while(c<len(inp)):
+  k=inp[c]
+  if(k>0):
+    c+=k+1
+    count+=1
+  else:
+    c+=1
+
+print(count,end=' ')
+print("Mission",end=' ')
+if (count<=maxi):
+  print("Successful")
+else:
+  print("Unsuccessful")
+
